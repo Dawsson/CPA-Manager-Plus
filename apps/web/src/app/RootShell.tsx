c8737e82d3332ffdc0ca9adedb159dab7a8c6b22
Dawson <hello@dawson.gg>
2026-06-11 10:21:24 -0400

    Use coss shell background

--- apps/web/src/app/RootShell.tsx
+++ apps/web/src/app/RootShell.tsx
@@ -1,17 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import { AppBackground } from '@/components/common/AppBackground';
 import { ConfirmationModal } from '@/components/common/ConfirmationModal';
 import { NotificationContainer } from '@/components/common/NotificationContainer';
 
 export function RootShell() {
   return (
-    <>
-      <AppBackground />
-      <div className="app-content isolate relative flex min-h-svh flex-col">
-        <NotificationContainer />
-        <ConfirmationModal />
-        <Outlet />
-      </div>
-    </>
+    <div className="app-content isolate relative flex min-h-svh flex-col">
+      <NotificationContainer />
+      <ConfirmationModal />
+      <Outlet />
+    </div>
   );
 }
